db12bad2bf235164d0b04f5921096058f57d824f
Rafael 'fumasa' Giorgetti <fumasa@wakedown.org>
2016-06-21 21:22:14 -0300

    fix standardjs

--- index.js
+++ index.js
@@ -1,60 +1,60 @@
-const LOGFILE = process.env.LOGFILE || __dirname + '/application.log'
+const LOGFILE = process.env.LOGFILE || './application.log'
 
 const util = require('util')
 const winston = require('winston')
 const logger = new winston.Logger()
-const production = (process.env.NODE_ENV || '').toLowerCase() === 'production'
+let production = (process.env.NODE_ENV || '').toLowerCase() === 'production'
 
 module.exports = {
-    middleware: function (req, res, next) {
-        console.info(req.method, req.url, res.statusCode)
-        next()
-    },
-    production: production
+  middleware: function (req, res, next) {
+    console.info(req.method, req.url, res.statusCode)
+    next()
+  },
+  production: production
 }
 
 // Override the built-in console methods with winston hooks
-switch((process.env.NODE_ENV || '').toLowerCase()) {
-    case 'production':
-        production = true;
-        logger.add(winston.transports.File, {
-            filename: LOGFILE,
-            json: true,
-            maxsize: 5242880, //5MB
-            maxFiles: 5,
-            colorize: false,
-            level: 'warn'
-        });
-        break
-    case 'test':
-        // Don't set up the logger overrides
-        return
-    default:
-        logger.add(winston.transports.Console, {
-            colorize: true,
-            timestamp: true,
-            exitOnError: false,
-            level: 'debug'
-        })
-        break
+switch ((process.env.NODE_ENV || '').toLowerCase()) {
+  case 'production':
+    production = true
+    logger.add(winston.transports.File, {
+      filename: LOGFILE,
+      json: true,
+      maxsize: 5242880, // 5MB
+      maxFiles: 5,
+      colorize: false,
+      level: 'warn'
+    })
+    break
+  case 'test':
+    // Don't set up the logger overrides
+    break
+  default:
+    logger.add(winston.transports.Console, {
+      colorize: true,
+      timestamp: true,
+      exitOnError: false,
+      level: 'debug'
+    })
+    break
 }
 
 function formatArgs (args) {
-    return [ util.format.apply(util.format, Array.prototype.slice.call(args)) ]
+  return [ util.format.apply(util.format, Array.prototype.slice.call(args)) ]
 }
 
 console.log = function () {
-    logger.info.apply(logger, formatArgs(arguments))
+  logger.info.apply(logger, formatArgs(arguments))
 }
 console.info = function () {
-    logger.info.apply(logger, formatArgs(arguments))
+  logger.info.apply(logger, formatArgs(arguments))
 }
 console.warn = function () {
-    logger.warn.apply(logger, formatArgs(arguments))
+  logger.warn.apply(logger, formatArgs(arguments))
 }
 console.error = function () {
-    logger.error.apply(logger, formatArgs(arguments))
+  logger.error.apply(logger, formatArgs(arguments))
 }
 console.debug = function () {
-    logger.debug.apply(logger, formatArgs(arguments))
+  logger.debug.apply(logger, formatArgs(arguments))
 }
